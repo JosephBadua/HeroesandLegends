ec841e2edd7faf2fde8eb7fa7c00ce82c8b53bce
Joseph <josephbadua1006@gmail.com>
2021-05-02 22:58:39 -0700

    landing page

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let first = document.getElementById('first-screen');
+    first.addEventListener('click', function () {
+        console.log('click');
+    });
+});
