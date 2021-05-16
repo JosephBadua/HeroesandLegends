669993c9f31ec97a868cb1c9c41dc348ba7159cf
Joseph <josephbadua1006@gmail.com>
2021-05-16 14:39:35 -0700

    checking main menu

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-// var mysql      = require('mysql');
+var mysql      = require('mysql');
 
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   port     :  3306 ,
-//   user     : 'root',
-//   password : 'Nightlyassassinx123!@#',
-//   database : 'HeroesAndLegends'
-// });
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  port     :  3306 ,
+  user     : 'root',
+  password : 'Nightlyassassinx123!@#',
+  database : 'HeroesAndLegends'
+});
  
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
- 
-//   console.log('connected as id ' + connection.threadId);
-// });
-
-// connection.query('SELECT * FROM announcements', function (error, results, fields) {
-//     if (error) throw error;
-//     console.log(results);
-//   });
+connection.connect();
 
 
 var app = express();
@@ -37,6 +25,30 @@ app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 require("./routes/htmlRoutes.js")(app);
+
+let category = [];
+
+app.post('/nomination_hold', function (req, res) {
+  if(category[0] != ''){
+    category.pop();
+  }
+
+  const name = req.body.category
+  connection.query('SELECT category_name FROM nomination_categories WHERE id = ' + name, function (error, results, fields) {
+    if (error) throw error;
+    let category_name = results[0].category_name;
+    // console.log(category_name);
+    category.push(category_name);
+    console.log(category);
+  });
+  
+
+  res.render("nomination_category");
+});
+
+app.get('/nomination_hold', (req, res) => {
+  res.json(category);
+});
 
 app.listen(PORT, function(){
     console.log("listening on: " + PORT)
